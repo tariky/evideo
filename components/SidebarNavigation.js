e1c1185f47a48744b7352a3c467415c02732d960
Tarik Caplja <tarik@lunatik.ba>
2022-03-03 20:10:50 +0100

    navigation route components created

--- components/SidebarNavigation.js
+++ components/SidebarNavigation.js
@@ -1,10 +1,12 @@
-import { ActionIcon, Group, Navbar, Text, UnstyledButton } from "@mantine/core";
-import { IoHome } from "react-icons/io5";
+import { ThemeIcon, Group, Navbar, Text, UnstyledButton } from "@mantine/core";
+import Link from "next/link";
+import { IoHome, IoPricetag } from "react-icons/io5";
+import { useState } from "react";
 
-export default function SidebarNavigation() {
+const RouteItemSingle = ({ title, url, icon, color }) => {
   return (
-    <Navbar width={{ base: 300 }} height={500}>
-      <UnstyledButton fullWidth>
+    <Link href={url}>
+      <UnstyledButton>
         <Group
           sx={(theme) => ({
             padding: "1rem",
@@ -14,12 +16,88 @@ export default function SidebarNavigation() {
             },
           })}
         >
-          <ActionIcon color="red" variant="filled">
-            <IoHome />
-          </ActionIcon>
-          <Text>Pocetna</Text>
+          <ThemeIcon color={color}>{icon}</ThemeIcon>
+          <Text>{title}</Text>
         </Group>
       </UnstyledButton>
+    </Link>
+  );
+};
+
+const RouteItemNested = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <UnstyledButton onClick={() => setIsOpen(!isOpen)}>
+        <Group
+          sx={(theme) => ({
+            padding: "1rem",
+            backgroundColor: theme.colors.gray[0],
+            "&:hover": {
+              backgroundColor: theme.colors.gray[1],
+            },
+          })}
+        >
+          <ThemeIcon color="blue">
+            <IoPricetag />
+          </ThemeIcon>
+          <Text>Proizvodi</Text>
+        </Group>
+      </UnstyledButton>
+      {isOpen ? (
+        <Group direction="column" spacing={"sm"} mt="md">
+          <Link href={"/"}>
+            <Text
+              weight={700}
+              sx={(theme) => ({
+                paddingLeft: "1rem",
+                width: "100%",
+              })}
+            >
+              Proizvodi
+            </Text>
+          </Link>
+          <Link href={"/"}>
+            <Text
+              weight={700}
+              sx={(theme) => ({
+                paddingLeft: "1rem",
+                width: "100%",
+              })}
+            >
+              Skladiste
+            </Text>
+          </Link>
+        </Group>
+      ) : null}
+    </>
+  );
+};
+
+export default function SidebarNavigation() {
+  const routes = [
+    {
+      title: "Komandna ploca",
+      url: "/",
+      icon: <IoHome />,
+      color: "red",
+    },
+  ];
+  return (
+    <Navbar width={{ base: 300 }} height={500}>
+      {routes.map((route, index) => {
+        return (
+          <RouteItemSingle
+            key={index}
+            title={route.title}
+            color={route.color}
+            icon={route.icon}
+            url={route.url}
+          />
+        );
+      })}
+      {/* Dev only */}
+      <RouteItemNested />
     </Navbar>
   );
 }
