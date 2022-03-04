685e0bb4389f413a42865ea95ea5e804175771d1
tariky <takibih@gmail.com>
2022-03-04 14:31:25 +0100

    new product page added

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,7 +1,20 @@
-import { Box, Button, Group, Title, Card, Input, Table } from "@mantine/core";
-import { IoSearch } from "react-icons/io5";
+import {
+  Box,
+  Button,
+  Group,
+  Title,
+  Card,
+  Input,
+  Table,
+  Image,
+  Checkbox,
+  ActionIcon,
+  Divider,
+} from "@mantine/core";
+import { IoArrowBack, IoArrowForward, IoSearch } from "react-icons/io5";
 
 export default function index() {
+  // Ovo se moze ukoloniti jer sam hardkodovo vrijednosti u rows konstanti
   const elements = [
     { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
     { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
@@ -11,10 +24,25 @@ export default function index() {
   ];
   const rows = elements.map((element) => (
     <tr key={element.name}>
-      <td>{element.position}</td>
-      <td>{element.name}</td>
-      <td>{element.symbol}</td>
-      <td>{element.mass}</td>
+      <td>
+        <Checkbox />
+      </td>
+      <td>
+        {/* Prikaz slike iz nekoga meni nepoznatnog razloga ima desnu marginu, potrebno dalje istrazivanje */}
+        <Image
+          radius="md"
+          src="https://cdn.shopify.com/s/files/1/0012/0707/4917/products/188_065d1763-cdfb-4621-89f5-d55ebd6bc090_350x350.jpg?v=1626683108"
+          alt="Random unsplash image"
+          width={50}
+          height={50}
+          fit="contain"
+        />
+      </td>
+      <td>Test123</td>
+      <td>Draft</td>
+      <td>5</td>
+      <td>Majica</td>
+      <td>Apple</td>
     </tr>
   ));
   return (
@@ -25,6 +53,7 @@ export default function index() {
       })}
     >
       <Box
+        my={9}
         sx={(theme) => ({
           display: "flex",
           flexDirection: "row",
@@ -37,6 +66,7 @@ export default function index() {
         <Button>Dodaj proizvod</Button>
       </Box>
       <Card
+        shadow={"sm"}
         sx={(theme) => ({
           width: "100%",
         })}
@@ -45,14 +75,28 @@ export default function index() {
         <Table mt={16}>
           <thead>
             <tr>
-              <th>Element position</th>
-              <th>Element name</th>
-              <th>Symbol</th>
-              <th>Atomic mass</th>
+              <th>
+                <Checkbox />
+              </th>
+              <th></th>
+              <th>Naziv</th>
+              <th>Status</th>
+              <th>Inventura</th>
+              <th>Tip</th>
+              <th>Dobavljac</th>
             </tr>
           </thead>
           <tbody>{rows}</tbody>
         </Table>
+        <Divider my={16} />
+        <Group direction="row" position="center" spacing={"xs"}>
+          <ActionIcon size="xl" radius="md" variant="filled">
+            <IoArrowBack />
+          </ActionIcon>
+          <ActionIcon size="xl" radius="md" variant="filled">
+            <IoArrowForward />
+          </ActionIcon>
+        </Group>
       </Card>
     </Group>
   );
---
+++ pages/products/yo.js
@@ -0,0 +1,62 @@
+import {
+  Grid,
+  Box,
+  Title,
+  Card,
+  Text,
+  Input,
+  Select,
+  Button,
+} from "@mantine/core";
+import { useState, React } from "react";
+// import { RichTextEditor } from "@mantine/rte";
+import { IoCloudUpload } from "react-icons/io5";
+
+export default function NewProduct() {
+  // const [value, onChange] = useState("<p>Test</p>");
+  return (
+    <Box>
+      <Title order={2} mb={16}>
+        Test
+      </Title>
+      <Grid gutter={16}>
+        <Grid.Col span={8}>
+          <Card shadow={"xs"}>
+            <Box>
+              <Text>Naziv</Text>
+              <Input />
+              <Text mt={10}>Opis</Text>
+              {/* NextJS iz nekog razloga ima problem sa ucitavanjem RTE editora istraziti detaljnije ili ga zamjeniti alternativom... mozda samo input obicni staviti */}
+              {/* <RichTextEditor value={value} onChange={onChange} /> */}
+              <Input />
+            </Box>
+          </Card>
+          <Card mt={16} shadow={"xs"}>
+            <Text>Fotografije</Text>
+            <Button fullWidth mt={16} leftIcon={<IoCloudUpload />}>
+              Ucitaj fotografiju
+            </Button>
+          </Card>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Card shadow={"xs"}>
+            <Text>Status</Text>
+            <Select
+              defaultValue="skica"
+              data={[
+                {
+                  value: "skica",
+                  label: "Skica",
+                },
+                {
+                  value: "aktivno",
+                  label: "Aktivno",
+                },
+              ]}
+            />
+          </Card>
+        </Grid.Col>
+      </Grid>
+    </Box>
+  );
+}
